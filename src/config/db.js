5dae3ef66e58b5698141bf70b157f9276750d648
BrunoRodrigues11 <bruno.ptr02@gmail.com>
2026-01-07 21:33:26 -0300

    refactor: database connection file

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASS),
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-});
+const isProduction = process.env.NODE_ENV === "production";
 
-console.log("🚀 Aplicação conectada ao PostgreSQL com êxito! ");
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: Number(process.env.DB_PORT),
+      }
+);
+
+// Teste REAL de conexão
+pool.query("SELECT 1")
+  .then(() => console.log("✅ PostgreSQL conectado"))
+  .catch(err => console.error("❌ Erro ao conectar no PostgreSQL", err));
+
 module.exports = pool;
